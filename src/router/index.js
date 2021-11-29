b50403c5a3ca94671b0dcc1e87e94fa5966f210d
Janos Papp <papp.janos.antal@gmail.com>
2021-11-29 11:36:56 +0100

    khj

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import { parse } from "@/util/jwt"
 
 Vue.use(VueRouter);
+
+function checkTokenAndUserRole(to, from, next) {
+  const sessionToken = window.sessionStorage.getItem('token')
+  if (!sessionToken) {
+    return next('/login')
+  }
+  const tokenPayload = parse(sessionToken)
+  console.log(tokenPayload)
+}
 
 const routes = [
   {
@@ -19,6 +29,12 @@ const routes = [
     path: "/login",
     name: "Login",
     component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/private",
+    name: "Private",
+    beforeEnter: checkTokenAndUserRole,
+    component: () => import("../views/Private.vue"),
   },
 ];
 
